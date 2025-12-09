4ff582dc0b9359ab546d630dd6cca322caa0645d
ThiagoPorfirio2003 <thiagolisandroporfirio@gmail.com>
2025-12-09 20:01:59 -0300

    1er commit

---
+++ src/main/java/com/porfirio/mycomerce/MyComerceApplication.java
@@ -0,0 +1,13 @@
+package com.porfirio.mycomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyComerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyComerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfirio/mycomerce/MyComerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfirio.mycomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyComerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
